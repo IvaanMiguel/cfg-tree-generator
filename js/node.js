9eded52490049a6e84a9c45fa77912c7f9605c42
Aivan <dominguezmiguel715@gmail.com>
2023-04-26 20:33:28 -0700

    _

---
+++ js/node.js
@@ -0,0 +1,10 @@
+class Node {
+  constructor (value) {
+    this.value = value;
+    this.children = [];
+    this.parentNode = null;
+    this.x = 0;
+    this.y = 0;
+    this.positionRelativeToParent = null;
+  }
+}
